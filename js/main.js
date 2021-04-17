fad89ef305ec620c1899c9c445e4d251232dc910
Naimur53 <naimurrhman53@gmail.com>
2021-04-17 15:09:15 +0600

    zoom done

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: '.grid-sizer'
+        }
+    })
+    $("#zoom_05").elevateZoom({
+        zoomType:"inner",
+        cursor: "crosshair"
+      });
+
+
+
+
+  })
